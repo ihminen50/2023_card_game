868a62ebea7340d9d03b21afcf9699ac8a8fea23
V.N <viljami@nisula.org>
2023-09-14 18:17:57 +0300

    css tehtiin

---
+++ card_game/src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./components/Card";
+import './App.css'
+
+export default function App(){
+  return(
+    <><h1>Hello world!</h1>
+     <Card/>
+    </>
+  );
+}
---
+++ card_game/src/components/Card.jsx
@@ -0,0 +1,14 @@
+
+export default function Card(){
+    return(
+       <div className="card">
+        <img src="http://placekitten.com/120/100"/>
+        <ul>
+            <li>
+                <span>Cuteness</span>
+                <span>10</span>
+            </li>
+        </ul>
+       </div>
+    );
+}
