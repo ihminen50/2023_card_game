e25bdd1bf0b9c2098ed424e856d34ebf9367a433
V.N <viljami@nisula.org>
2023-09-21 18:14:38 +0300

    tehtiin css loppuun

--- card_game/src/components/Card.jsx
+++ card_game/src/components/Card.jsx
@@ -3,8 +3,8 @@ export default function Card(){
     return(
        <div className="card">
         <img src="http://placekitten.com/120/100"/>
-        <ul>
-            <li>
+        <ul className="stat-list">
+            <li className="stat-list-item">
                 <span>Cuteness</span>
                 <span>10</span>
             </li>
