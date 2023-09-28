8f090f37678a9593d1ef956a1832e8a5e1220022
V.N <viljami@nisula.org>
2023-09-28 18:17:01 +0300

    mappi aloitettiin

--- card_game/src/App.jsx
+++ card_game/src/App.jsx
@@ -1,10 +1,18 @@
 import Card from "./components/Card";
 import './App.css'
 
+const playerCard = {
+  image: 'http://placekitten.com/120/100',
+  stats: [{name: 'Cutness', value: 99},
+  {name: 'speed', value: 555}
+  
+]
+}         
+
 export default function App(){
   return(
-    <><h1>Hello world!</h1>
-     <Card/>
+    <><h1>kissa peli</h1>
+     <Card card={playerCard}/>
     </>
   );
 }
