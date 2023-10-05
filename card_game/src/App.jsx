5e7e229d2bfc7a95b6d8b92bac55ca26ca0e57b9
V.N <viljami@nisula.org>
2023-10-05 18:16:52 +0300

    vastustajan kortti

--- card_game/src/App.jsx
+++ card_game/src/App.jsx
@@ -7,12 +7,29 @@ const playerCard = {
   {name: 'speed', value: 555}
   
 ]
-}         
+}      
+
+const opponentCard = {
+  image: 'http://placekitten.com/120/100',
+  stats: [{name: 'Cutness', value: 66},
+  {name: 'speed', value: 234}
+  
+]
+}  
 
 export default function App(){
+
+function compareCards(){
+  console.log("button clicked!");
+}
+
   return(
     <><h1>kissa peli</h1>
+     <div className= 'game'> 
      <Card card={playerCard}/>
+     <button onClick={compareCards} type="button">Play</button>
+     <Card card={opponentCard}/>
+     </div>
     </>
   );
 }
